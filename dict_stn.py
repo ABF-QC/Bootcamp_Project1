661fb93a8fa5fe2d17f377ab3341ca8e021c8a4e
Anna-Belle Filion <treblecharger34@hotmail.com>
2025-02-11 13:59:53 -0500

    added dictionary

---
+++ dict_stn.py
@@ -0,0 +1,34 @@
+stn_reports = {
+    'hly_dly_mly' : {
+        'name' : 'HLY/DLY/MLY',
+        'color' : 'purple',
+    },
+    'hly_dly' : {
+        'name' : 'HLY/DLY',
+        'color' : 'blue',
+    },
+    'hly_mly' : {
+        'name' : 'HLY/MLY',
+        'color' : 'black',
+    },
+    'hly' : {
+        'name' : 'HLY',
+        'color' : 'red',
+    },
+    'dly_mly' : {
+        'name' : 'DLY/MLY',
+        'color' : 'orange',
+    },
+    'dly' : {
+        'name' : 'DLY',
+        'color' : 'lightblue',
+    },
+    'mly' : {
+        'name' : 'MLY',
+        'color' : 'yellow',
+    },
+    'other' : {
+        'name' : 'no reports',
+        'color' : 'pink',
+    },
+}
